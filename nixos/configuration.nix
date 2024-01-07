a3ce5926fd6fbd387d08cc5c51c74733bb784513
Rizqirazkafi <rizqirazkafi56@gmail.com>
2024-01-07 20:56:06 +0700

    Networks, Tmux, hostname
    Configure static IP address, adjust tmux config to use full color, change hostname to nixos-vm so it won't confuse with local nixos

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -6,26 +6,30 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      # Include the results of the hardware scan.
       ./hardware-configuration.nix
-	./vim.nix
-  # ./disk-config.nix
-    inputs.home-manager.nixosModules.home-manager
+      ./vim.nix
+      # ./disk-config.nix
+      inputs.home-manager.nixosModules.home-manager
     ];
-	# Added flakes
-	nix.settings.experimental-features = [ "nix-command" "flakes" ];
-	nix.package = pkgs.nixFlakes;
+  # Added flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.package = pkgs.nixFlakes;
 
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
-    extraSpecialArgs = {inherit inputs;};
+    extraSpecialArgs = { inherit inputs; };
     users.rizqirazkafi = import ../home.nix;
   };
   programs.tmux = {
     enable = true;
     extraConfig = ''
+      set -g default-terminal "tmux-256color"
       set-option -sa terminal-overrides ",xterm*:Tc"
+      set-option -sa terminal-overrides ",alacritty:RGB"
+
       # bind <C-Alt-*> to move arround windows
       bind -n C-M-H next-window
       bind -n C-M-L previous-window
@@ -50,7 +54,10 @@
   # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixos-vm"; # Define your hostname.
+  networking.interfaces.ens18.ipv4.addresses = [{ address = "192.168.30.60"; prefixLength = 25; }];
+  networking.defaultGateway.address = "192.168.30.1";
+  networking.nameservers = [ "1.1.1.1" ];
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
@@ -74,7 +81,7 @@
   # services.xserver.enable = true;
 
 
-  
+
 
   # Configure keymap in X11
   # services.xserver.layout = "us";
@@ -86,52 +93,55 @@
   # Enable sound.
   # sound.enable = true;
   # hardware.pulseaudio.enable = true;
-nixpkgs.config.pulseaudio = true;
+  nixpkgs.config.pulseaudio = true;
 
-services.xserver = {
-	enable = true;
-	desktopManager = {
-		xterm.enable = false;
-		xfce.enable = true;
+  services.xserver = {
+    enable = true;
+    desktopManager = {
+      xterm.enable = false;
+      xfce.enable = true;
 
-	};
-	displayManager.defaultSession = "xfce";
-};
+    };
+    displayManager.defaultSession = "xfce";
+  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-   users.users.rizqirazkafi = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-     packages = with pkgs; [
-       firefox
-       tree
-     ];
-     openssh.authorizedKeys.keys = [
-       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH8rh5OxRHsKPT/ic6qA7G3VxqV9SVFW89CdLnVPDJNY rizqirazkafi56@gmail.com"
-     ];
+  users.users.rizqirazkafi = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      firefox
+      tree
+    ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH8rh5OxRHsKPT/ic6qA7G3VxqV9SVFW89CdLnVPDJNY rizqirazkafi56@gmail.com"
+    ];
 
-   };
-   users.users.root.openssh.authorizedKeys.keys = [
-     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH8rh5OxRHsKPT/ic6qA7G3VxqV9SVFW89CdLnVPDJNY rizqirazkafi56@gmail.com"
-   ];
+  };
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH8rh5OxRHsKPT/ic6qA7G3VxqV9SVFW89CdLnVPDJNY rizqirazkafi56@gmail.com"
+  ];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-   environment.systemPackages = with pkgs; [
-     lazygit
-     ansible
-     tmux
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-	neovim
-     wget
-		 git
-		 home-manager
-		 htop
-		 alacritty
-   ];
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    lazygit
+    ansible
+    tmux
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    neovim
+    wget
+    git
+    home-manager
+    htop
+    alacritty
+    zerotierone
+  ];
+  services.zerotierone.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -145,10 +155,10 @@ services.xserver = {
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-  services.openssh.ports = [9005];
+  services.openssh.ports = [ 9005 ];
 
   # Open ports in the firewall.
-   networking.firewall.allowedTCPPorts = [9005];
+  networking.firewall.allowedTCPPorts = [ 9005 9993 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   #  networking.firewall.enable = false;

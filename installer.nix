acfef042ae2898ac1b31444c988a277f7cea5b53
Rizqirazkafi <rizqirazkafi56@gmail.com>
2024-01-14 10:07:29 +0700

    Removed installer.nix
    Using regular bash script for easy writting

--- installer.nix
+++
@@ -1,5 +0,0 @@
-{pkgs}:
-pkgs.writeShellScriptBin "nix-auto-installer" ''
-  echo "Welcome to the NixOS installer script\n"
-  echo "hello world" | ${pkgs.cowsay}/bin/cowsay | ${pkgs.lolcat}/bin/lolcat
-''

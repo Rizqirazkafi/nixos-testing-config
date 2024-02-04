688b962560a300c50b511fa4305014fe68aaeb52
Rizqirazkafi <rizqirazkafi56@gmail.com>
2024-02-04 19:49:04 +0700

    Removed comments and reformat

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -5,18 +5,16 @@
 { inputs, config, pkgs, ... }:
 
 {
-  imports =
-    [
-      # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./vim.nix
-      # ./disk-config.nix
-      inputs.home-manager.nixosModules.home-manager
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./vim.nix
+    # ./disk-config.nix
+    inputs.home-manager.nixosModules.home-manager
+  ];
   # Added flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.package = pkgs.nixFlakes;
-
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
@@ -29,66 +27,26 @@
       set -g default-terminal "tmux-256color"
       set-option -sa terminal-overrides ",xterm*:Tc"
       set-option -sa terminal-overrides ",alacritty:RGB"
-
-      # bind <C-Alt-*> to move arround windows
-      bind -n C-M-H next-window
-      bind -n C-M-L previous-window
-      # Set prefix to <C-Space>
       unbind C-b
       set -g prefix C-Space
       bind C-Space send-prefix
       set -g mouse
     '';
-    plugins = with pkgs.tmuxPlugins ; [
-      vim-tmux-navigator
-      sensible
-    ];
+    plugins = with pkgs.tmuxPlugins; [ vim-tmux-navigator sensible ];
   };
-
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
   networking.hostName = "nixos-vm"; # Define your hostname.
   # networking.interfaces.ens18.ipv4.addresses = [{ address = "192.168.30.60"; prefixLength = 25; }];
   # networking.defaultGateway.address = "192.168.30.1";
   networking.nameservers = [ "1.1.1.1" ];
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Asia/Jakarta";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkbOptions in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-
-
-
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
 
   # Enable sound.
   # sound.enable = true;
@@ -116,10 +74,7 @@
   users.users.rizqirazkafi = {
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      firefox
-      tree
-    ];
+    packages = with pkgs; [ firefox tree ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH8rh5OxRHsKPT/ic6qA7G3VxqV9SVFW89CdLnVPDJNY rizqirazkafi56@gmail.com"
     ];
@@ -136,7 +91,7 @@
     lazygit
     ansible
     tmux
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     neovim
     wget
     git
@@ -146,14 +101,6 @@
   ];
   services.qemuGuest.enable = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
@@ -161,23 +108,7 @@
   services.openssh.ports = [ 9005 ];
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 9005 9993 3389 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  #  networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+  networking.firewall.allowedTCPPorts = [ 9005 ];
+  system.stateVersion = "23.05";
 
 }
-

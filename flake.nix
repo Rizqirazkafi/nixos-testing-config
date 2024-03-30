bad712486274e70d59167997628ed53307fb2b49
Rizqirazkafi <rizqirazkafi56@gmail.com>
2024-03-30 14:14:02 +0700

    remove unnecessary inputs
    remove latex and nix-colors

--- flake.nix
+++ flake.nix
@@ -8,8 +8,8 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-colors.url = "github:misterio77/nix-colors";
-    latex.url = "github:nixos/nixpkgs/2d627a2a704708673e56346fcb13d25344b8eaf3";
+    # nix-colors.url = "github:misterio77/nix-colors";
+    # latex.url = "github:nixos/nixpkgs/2d627a2a704708673e56346fcb13d25344b8eaf3";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
@@ -17,12 +17,9 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
-        config = {
-          allowUnfree = true;
-        };
+        config = { allowUnfree = true; };
       };
-    in
-    {
+    in {
       nixosConfigurations = {
         nixos-vm = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs system; };
@@ -34,4 +31,3 @@
       };
     };
 }
-

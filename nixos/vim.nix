8a1f7e6067b0579c95ebca4b3cb2e438146177bb
Rizqirazkafi <rizqirazkafi56@gmail.com>
2024-06-05 08:12:36 +0700

    remove vim plugin

--- nixos/vim.nix
+++ nixos/vim.nix
@@ -4,10 +4,10 @@
       ((vim_configurable.override { }).customize {
         name = "vim";
         # Install plugins for example for syntax highlighting of nix files
-        vimrcConfig.packages.myplugins = with pkgs.vimPlugins; {
-          start = [ vim-nix vim-lastplace ];
-          opt = [ ];
-        };
+        # vimrcConfig.packages.myplugins = with pkgs.vimPlugins; {
+        #   start = [ vim-nix vim-lastplace ];
+        #   opt = [ ];
+        # };
         vimrcConfig.customRC = ''
                   " your custom vimrc
           				set nu
